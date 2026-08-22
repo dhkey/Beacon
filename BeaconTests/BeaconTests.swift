70f4dfc79864d3c6d45b2ceef667a879a25c56cd
dhkey <dhkey14@icloud.com>
2026-08-22 13:05:15 +0200

    Initial Commit

---
+++ BeaconTests/BeaconTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconTests.swift
+//  BeaconTests
+//
+//  Created by Denys Yazan on 22.08.2026.
+//
+
+import Testing
+@testable import Beacon
+
+struct BeaconTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
